075c8254993808d40723912947fb427af8dc2146
FelipeRojasC <103686460+FelipeRojasC@users.noreply.github.com>
2022-10-06 22:06:49 -0300

    RegistrarCliente

--- src/main/java/cl/nessfit/web/model/Rol.java
+++ src/main/java/cl/nessfit/web/model/Rol.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -18,7 +19,7 @@ public class Rol implements Serializable{
 	private int id;
 	private String nombre;
 	
-	@OneToOne(mappedBy = "rol")
+	//@OneToMany(mappedBy = "rol")
 	private Usuario usuario;
 
 	public int getId() {
