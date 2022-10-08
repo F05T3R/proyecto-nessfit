8a0afa85800a9aae5a71f8515e3b85cbde24d01c
FelipeRojasC <103686460+FelipeRojasC@users.noreply.github.com>
2022-10-07 23:26:45 -0300

    Correccion de errores
    Version mas actualizada del proyecto

--- src/main/java/cl/nessfit/web/controller/RegistrarClienteController.java
+++ src/main/java/cl/nessfit/web/controller/RegistrarClienteController.java
@@ -7,7 +7,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -15,6 +19,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import cl.nessfit.web.model.Rol;
 import cl.nessfit.web.model.Usuario;
 import cl.nessfit.web.service.CUsuarioService;
+import cl.nessfit.web.utils.validacionUsuario;
 
 @Controller
 @RequestMapping(value="/administrativo")
@@ -26,6 +31,14 @@ public class RegistrarClienteController {
     @Autowired
     BCryptPasswordEncoder passwordEncoder;
     
+    @Autowired
+    private validacionUsuario validacionUsuario;
+    
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+    	binder.addValidators(validacionUsuario);
+    }
+    
     @RequestMapping(value="/RegistrarCliente", method=RequestMethod.GET)
     public String RegistrarCliente(Usuario usuario) {
     	return "administrativo/RegistrarCliente";
@@ -35,14 +48,14 @@ public class RegistrarClienteController {
     public String formCrearUsuario(@Valid Usuario usuario, BindingResult result, RedirectAttributes attr) {
 
 	// paso 1 validaciones
-	//result.rejectValue("rut", null, "rut inválido");
     	
-    	Usuario existe = usuarioService.buscarPorRut(usuario.getRut());
+    Usuario existe = usuarioService.buscarPorRut(usuario.getRut());
 
     if (existe != null) {
     	result.rejectValue("rut", null, "Rut existente en la base de datos");
     }
     	
+    
 	if (result.hasErrors()) {
 	    return "/administrativo/RegistrarCliente";
 	}
@@ -50,9 +63,9 @@ public class RegistrarClienteController {
 	// paso 2 set atributos no ingresados por usuario
 	usuario.setContrasena(passwordEncoder.encode(usuario.getRut()));
 	usuario.setEstado(1);
-	Rol rolAdministrativo = new Rol();
-	rolAdministrativo.setId(3);
-	usuario.setRol(rolAdministrativo);
+	Rol rolCliente = new Rol();
+	rolCliente.setId(3);
+	usuario.setRol(rolCliente);
 	System.out.println(usuario.toString());
 
 	// paso 3 persistencia 
@@ -64,7 +77,7 @@ public class RegistrarClienteController {
 
     @ModelAttribute("rutUser")
     public String auth() {
-    	Usuario usuario = usuarioService.buscarPorRut(SecurityContextHolder.getContext().getAuthentication().getName());
-    	return usuario.getNombre();
+
+	return SecurityContextHolder.getContext().getAuthentication().getName();
     }
 }
