546b147a9d3138c09079b012979e90b8e3d472da
Benjh4 <114027598+Benjh4@users.noreply.github.com>
2022-10-09 00:31:47 -0300

    Validaciones PT2
    mapeo cierra sesion, se agrega el registrar administrativo, y validaciones extras

--- src/main/java/cl/nessfit/web/controller/HomeController.java
+++ src/main/java/cl/nessfit/web/controller/HomeController.java
@@ -29,20 +29,59 @@ public class HomeController {
 	@Autowired
     BCryptPasswordEncoder passwordEncoder;
 	
+	@RequestMapping(value = {"/InicioSesion",""}, method = RequestMethod.GET)
+	public String InicioSesion() {
+		return "InicioSesion";
+	}
+	
+	@GetMapping("/Registrar")
+	public String Registrar() {
+		return "Registrar";
+	}
+	
 	@GetMapping("/MenuPrincipal")
 	public String MenuPrincipal() {
 		return "MenuPrincipal";
 	}
+	
+	@GetMapping("/logout")
+    public String logout(HttpServletRequest request) {
+	SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
+	logoutHandler.logout(request, null, null);
+	return "redirect:/";
+    }
 	
 	@ModelAttribute("rolUser")
     public String rol() {
     return SecurityContextHolder.getContext().getAuthentication().getAuthorities().stream().findFirst().get()
         .getAuthority();
     }
-	@ModelAttribute("rutUser")
-    public String auth() {
-    	Usuario usuario = usuarioService.buscarPorRut(SecurityContextHolder.getContext().getAuthentication().getName());
-    	return usuario.getNombre();
-    }
+	
+	@PostMapping("/perfil")
+	public String perfilForm(@Valid Usuario usuario, BindingResult result, Model model) {
+		Usuario usuarioAuth = usuarioService
+			.buscarPorRut(SecurityContextHolder.getContext().getAuthentication().getName());
 
+		// Paso 1.-Validaciones
+		if (result.hasErrors()) {
+		    return "perfil";
+		}
+
+		// Paso 2.- Set´s
+		usuarioAuth.setNombre(usuario.getNombre());
+		usuarioAuth.setApellido(usuario.getApellido());
+		usuarioAuth.setEmail(usuario.getEmail());
+		usuarioAuth.setTelefono(usuario.getTelefono());
+
+		// Paso 3.- Persistencia
+		usuarioService.guardar(usuarioAuth);
+
+		// Paso 4.- Redireccion
+		return "redirect:/";
+	    }
+
+	    @ModelAttribute("rutUser")
+	    public String auth() {
+		return SecurityContextHolder.getContext().getAuthentication().getName();
+	    }
 }
