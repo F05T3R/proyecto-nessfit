f14002c97cf1960f6ea69996f7555f6887ac0a33
Benjh4 <114027598+Benjh4@users.noreply.github.com>
2022-10-31 21:50:54 -0300

    AGREGADO PARCIALMENTE INSTALACION

---
+++ src/main/java/cl/nessfit/web/repository/InstalacionDeportivaRepository.java
@@ -0,0 +1,11 @@
+package cl.nessfit.web.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import cl.nessfit.web.model.InstalacionDeportiva;
+
+
+public interface InstalacionDeportivaRepository extends JpaRepository<InstalacionDeportiva, String>{
+	
+	
+}
