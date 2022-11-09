15f227b0407b3929589b36b1e6cfe511e96509ef
FelipeRojasC <103686460+FelipeRojasC@users.noreply.github.com>
2022-11-08 22:43:28 -0300

    Arrendar Centro
    -Calendario

---
+++ src/main/java/cl/nessfit/web/repository/SolicitudRepository.java
@@ -0,0 +1,10 @@
+package cl.nessfit.web.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import cl.nessfit.web.model.Solicitud;
+
+public interface SolicitudRepository extends JpaRepository<Solicitud,String> {
+
+	
+}
