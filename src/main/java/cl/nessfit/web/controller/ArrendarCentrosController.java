eba77eabbad187b807e5efa9b6466fedee2d627e
FelipeRojasC <103686460+FelipeRojasC@users.noreply.github.com>
2022-11-13 12:01:30 -0300

    ArrendarCentroController

--- src/main/java/cl/nessfit/web/controller/ArrendarCentrosController.java
+++ src/main/java/cl/nessfit/web/controller/ArrendarCentrosController.java
@@ -2,7 +2,6 @@ zpackage cl.nessfit.web.controller;
 
 import java.util.List;
 
-
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
@@ -18,9 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import cl.nessfit.web.model.DetalleSolicitud;
-import cl.nessfit.web.model.Solicitud;
-import cl.nessfit.web.model.Usuario;
 import cl.nessfit.web.model.FechasSolicitud;
 import cl.nessfit.web.model.InstalacionDeportiva;
 import cl.nessfit.web.model.Solicitud;
@@ -41,31 +37,9 @@ public class ArrendarCentrosController {
 	private int num_id;
 
 	@Autowired
-    CSolicitudService solicitudService;
-	
-	@Autowired
     CUsuarioService usuarioService;
 
 	
-	 
-	  @GetMapping("/ArrendarCentro")
-	    public String FormObtener(Model model) {
-		return "cliente/ArrendarCentros";
-	    }
-	  @PostMapping("/ArrendarCentro")
-	    public String FormArrendarSolcitud(HttpServletRequest request, @Valid Solicitud solicitud ,BindingResult result, Model model) {
-		 // DetalleSolicitud detalleSolicitud = new DetalleSolcitud();
-	  if(request.getParameterValues("dia") != null) {
-		  for(String dia : request.getParameterValues("dia")) {
-			  System.out.println(dia);
-		  }
-		  
-	  } 
-		// 1.- usuario logeado
-			Usuario usuario = usuarioService.buscarPorRut(SecurityContextHolder.getContext().getAuthentication().getName());
-		  
-		  return "redirect:ArrendarCentro";
-	  }
 	@Autowired
 	CInstalacionDeportivaService InstalacionDeportivaService;
 	
