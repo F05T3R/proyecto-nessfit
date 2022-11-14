f70f3e58c57be9752a08f0d86f38b93f6c846cd2
F05T3R <83479444+F05T3R@users.noreply.github.com>
2022-11-13 21:24:49 -0300

    Revert "ArrendarCentroController"
    This reverts commit 796ee158aeba79797d88da98af7a19662d87915f.

--- src/main/java/cl/nessfit/web/service/FechasSolicitudService.java
+++
@@ -1,8 +0,0 @@
-package cl.nessfit.web.service;
-
-import cl.nessfit.web.model.FechasSolicitud;
-
-public interface FechasSolicitudService {
-
-	public void guardar(FechasSolicitud fechasSolicitud);
-}
