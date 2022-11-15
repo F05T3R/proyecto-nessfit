43c863ae48ec6123620d9a9fad74024f4f717b06
FelipeRojasC <103686460+FelipeRojasC@users.noreply.github.com>
2022-11-15 15:25:49 -0300

    Proyecto actualizado

--- src/main/java/cl/nessfit/web/service/SolicitudService.java
+++ src/main/java/cl/nessfit/web/service/SolicitudService.java
@@ -4,6 +4,7 @@ import cl.nessfit.web.model.Solicitud;
 
 public interface SolicitudService {
 
-	
 	public void guardar(Solicitud solicitud);
+	public Solicitud buscarPorId(int id);
+	public void eliminar(int id);
 }
