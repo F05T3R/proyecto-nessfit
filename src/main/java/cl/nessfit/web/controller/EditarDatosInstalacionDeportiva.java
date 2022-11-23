f3a76850f223b80a09dff524faf99f5a74dde38a
FelipeRojasC <103686460+FelipeRojasC@users.noreply.github.com>
2022-11-23 17:35:57 -0300

    modificaciones

--- src/main/java/cl/nessfit/web/controller/EditarDatosInstalacionDeportiva.java
+++ src/main/java/cl/nessfit/web/controller/EditarDatosInstalacionDeportiva.java
@@ -1,5 +1,4 @@
 package cl.nessfit.web.controller;
-
 import java.util.List;
 
 import javax.validation.Valid;
@@ -69,7 +68,15 @@ public class EditarDatosInstalacionDeportiva {
 	@RequestMapping(value = {"/editar/{nombre}"}, method = RequestMethod.POST)
 	public String formEditar(@Valid InstalacionDeportiva instalacion, BindingResult result, RedirectAttributes attr, Model model) {
 		
-		System.out.println("2");
+		if(instalacion.getEstado() == null) {
+           
+            result.rejectValue("estado", null, "Estado no válido");
+        }
+        else {
+            if(instalacion.getEstado() != 1  && instalacion.getEstado() !=  0 ) {
+                result.rejectValue("estado", null, "Estado no válido");
+            }
+        }
 
 		if (instalacion.getDireccion() == "") {
 			result.rejectValue("direccion", null, "Complete este campo ");
@@ -83,7 +90,13 @@ public class EditarDatosInstalacionDeportiva {
 		if(instalacion.getEstado() != 1  && instalacion.getEstado() !=  0 ) {
 			result.rejectValue("estado", null, "Estado no válido");
 		}
-		
+		if (instalacion.getNombre().isBlank()) {
+            result.rejectValue("nombre", null, "Complete este campo ");
+        }
+
+        if (instalacion.getDireccion().isBlank()) {
+            result.rejectValue("direccion", null, "Complete este campo ");
+        }
 		if (result.hasErrors()) {
 			model.addAttribute("tiposInstalaciones", TipoInstalacion.values());
 		    return "/administrativo/editar_Instalacion";
--- src/main/resources/static/js/Calendario.js
+++ src/main/resources/static/js/Calendario.js
@@ -55,6 +55,8 @@ EscribirMes(numeroMes);
 mostrarMensaje();
 
 function EscribirMes(mes){
+	contador = 0;
+	mostrarMensaje();
 	for(let i = ComienzoSemana(); i>0;i--){
         fecha.innerHTML += ` <div class="diaDeshabilitado">
             ${getTotalDias(numeroMes-1)-(i-1)}
@@ -163,15 +165,11 @@ function getValor() {
 	valorFinal = valorFinal*contador;
 }
 function mostrarMensaje(){
-	var mensaje;
 	if(contador == 0){
-		mensaje =`Seleccione uno o mas días`
-		
+		var mensaje =`No hay fechas elegidas.`
+	}else{
+		var mensaje =`¿Está seguro de proceder con la solicitud de arriendo? El costo total de arriendo es de $${valorFinal}.`
 	}
-	else{
-		mensaje =`¿Está seguro de proceder con la solicitud de arriendo? El costo total de arriendo es de $${valorFinal}.`
-	}
-	
 	var etiqueta= document.getElementById("mensaje").innerHTML = mensaje;
 }
 function ExisteFecha(diaBuscado,mesBuscado,añoBuscado){
