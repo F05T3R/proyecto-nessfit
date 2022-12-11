f4c8422cbb3641caa7efd1dd6b1c7cd12c01a4b6
FelipeRojasC <103686460+FelipeRojasC@users.noreply.github.com>
2022-12-10 23:15:12 -0300

    Actualización
    Corrección de css
    proyecto funcional para hacer pruebas

--- src/main/java/cl/nessfit/web/service/FechaSolicitudService.java
+++
@@ -1,9 +0,0 @@
-package cl.nessfit.web.service;
-
-import cl.nessfit.web.model.FechasSolicitud;
-
-public interface FechaSolicitudService {
-	public void guardar(FechasSolicitud fechasSolicitud);
-	public FechasSolicitud buscar(int id, String fecha);
-	public void eliminar(FechasSolicitud delete);
-}
