290abf14ec56fa96fea914af65fba49c4a89b247
FelipeRojasC <103686460+FelipeRojasC@users.noreply.github.com>
2022-12-19 18:18:35 -0300

    Corrección editar Instalaciones

--- src/main/java/cl/nessfit/web/model/InstalacionDeportiva.java
+++ src/main/java/cl/nessfit/web/model/InstalacionDeportiva.java
@@ -9,6 +9,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
